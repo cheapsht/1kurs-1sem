9cf14a2aaeb536eb424a5b3d2e0b79afd96be980
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2025-10-13 13:53:53 +0300

    2-2.c

---
+++ 2-2.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+/**
+ @brief расчитывает значение функции Y
+ @param a значение переменной a
+ @param x знаяение переменной x
+ @return расчитывает значение
+double getY(double const a, double const x);
+ */
+double getY(double const a,double const x);
+/**
+ * @brief считывает значение,
+ * введенное с клавиатуры с проверкой ввода
+ * @return считанное значение
+ */
+double getValue();
+/**
+ * @brief Точка входа в программу
+ * @return возвращает 0, если программма выполнена корректно
+ */
+int main()
+{
+    double a = 2.5;
+    printf("Введите перменную x : ");
+    double x = getValue();
+    printf("Результат : %.4lf\n",getY(a,x));
+    return 0;
+}
+double getY(double const a,double const x)
+{
+    if (x<0.5)
+    {
+        return pow(a * x + 1, 1.0/3.0);
+    }
+    else if  (x == 0.5)
+    {
+        return a*x + 3;
+    }
+    else
+    {
+        return pow((pow(a,2)*x - 1),0.5);
+    }
+}
+double getValue()
+{
+    double value = 0;
+    if (!scanf("%4lf",&value))
+    {
+        printf("Error\n");
+        abort();
+    }
+    return value;
+}
