529b77ca1649ec386a7cb84f7b2fe9f12a762d4a
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2025-11-18 19:34:25 +0300

    4-1.c

---
+++ 4-1.c
@@ -0,0 +1,231 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @brief считывает целое значение с клавиатуры в проверкой ввода
+ * @return возвращает считанное значение
+ */
+int Value();
+/**
+ * @brief считывает размер с клавиатуры с проверкой ввода и корректности значения
+ * @param message сообщение для пользователя, которое выводится перед вводом
+ * @return возвращает считанное значение типа size_t
+ */
+size_t getSize(char* message);
+/**
+ * @brief заполняет массив числами,введенными с клавиатуры
+ * @param arr указатель на массив для заполнения
+ * @param size размер массива
+ */
+void fillArray(int* arr, const size_t size);
+/**
+ * @brief выводит наш массив
+ * @param arr указатель на массив для заполнения
+ * @param size размер массива
+ */
+void printArray(int* arr, const size_t size);
+/**
+ * @brief заполняет массив случайнвми числами
+ * @param arr указатель на массив для заполнения
+ * @param size размер массива
+ */
+void fillRandom(int* arr, const size_t size);
+/**
+ * @brief находит четные,по модулю не превосходящие 5 числа и перемножяет их
+ * @param arr указатель на массив для заполнения
+ * @param size размер массива
+ */
+void findChetElementsMoreFive (int* arr, const size_t size);
+/**
+ * @brief создает копию массива
+ * @param arr указатель на исходный массив
+ * @param size размер массива
+ * @return указатель на новый массив-копию
+ */
+int* copyArray(const int* arr, const size_t size);
+/**
+ * @brief находит нечетные,по модулю превосходящие параметр А числа и считает их количество
+ * @param arr указатель на массив для заполнения
+ * @param size размер массива
+ */
+void EX2(int* arr, const size_t size);
+/**
+ * @brief находит нечетные,кратные трем элементы и умножает их значение на порядковый номер
+ * @param arr указатель на массив для заполнения
+ * @param size размер массива
+ */
+void EX3(int* arr, const size_t size);
+/**
+@brief RANDOM -  заполняет массив случайными числами
+@brief MANUAL - заполняет массив числами введенными с клавиатуры
+*/
+enum {RANDOM = 1, MANUAL = 2};
+/**
+ * @brief Точка входа в программу
+ * @return 0, если программа выполнена корректно, иначе 1
+ */
+int main()
+{
+    printf("Введите параметр A : ");
+    int A = Value();
+    size_t size = getSize("Введите размер массива:  ");
+    int* arr = malloc(size * sizeof(int));
+    if (arr == NULL)
+    {
+        printf("error");
+        exit(1);
+    }
+    printf("Выберите способ заполнения массива:\n"
+            "%d случайными числамиб %d вручную ", RANDOM, MANUAL);
+    int choice = Value();
+    switch(choice)
+    {
+        case RANDOM:
+            fillRandom(arr, size);
+            break;
+        case MANUAL:
+            fillArray(arr, size);
+            break;
+        default:
+            printf("error");
+            free(arr);
+            break;
+    }
+    printArray(arr, size);
+    findChetElementsMoreFive (arr,size);
+    EX2(arr,size);
+    EX3(arr,size);
+
+    int* copyArr = copyArray(arr,size);
+    return 0;
+}
+int Value()
+{
+    int value = 0;
+    if (!scanf("%d", &value))
+    {
+        printf("ERROR\n");
+        abort();
+    }
+    return (size_t)value;
+}
+void fillArray(int* arr, const size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("Vvedite AA = ");
+        arr[i] = Value();
+    }
+}
+void printArray(int* arr, const size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n ");
+}
+void fillRandom(int* arr, const size_t size)
+{
+    printf("diapozon start ");
+    int start = Value();
+    printf("diapozon end ");
+    int end = Value();
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = (rand() % (end - start + 1)) + start;
+    }
+}
+int* copyArray(const int* arr, const size_t size)
+{
+    int* copyArr = malloc(sizeof(int)*size);
+    for (size_t i =0; i<size; i++)
+    {
+        copyArr[i] = arr[i];
+    }
+    return copyArr;
+}
+size_t getSize(char* message)
+{
+    printf("%s", message);
+    int value = Value();
+    if (value <= 0)
+    {
+        printf("ERROR");
+        abort();
+    }
+    return (size_t)value;
+}
+void findChetElementsMoreFive (int* arr, const size_t size)
+{
+    int result = 1;
+    int found = 0;
+    for  (size_t i = 0; i<size; i++)
+    {
+        if (arr[i] % 2 == 0 && abs(arr[i]) < 5)
+        {
+            printf("задание 1) найдены четные элементы ,по модулю не превосходящие 5 :  %d\n ", arr[i]);
+            result = result * arr[i];
+            found = 1;
+        }
+    }
+    if (found)
+    {
+        printf("задание 1) произведение чётных чисел по модулю меньше 5 : %d\n",result);
+    }
+    else
+    {
+        printf("задание 1) Подходящие элементы не найдены\n");
+    }
+    
+}
+void EX2(int* arr, const size_t size)
+{
+    int A = 0;
+    int count = 0;
+    int found = 0;
+    for  (size_t i = 0; i<size; i++)
+    {
+        if (arr[i] % 2 != 0 && abs(arr[i]) > A)
+        {
+            printf("задание 2) найдены нечетные элементы, по модулю превосходящие A : %d\n",arr[i]);
+            count++;
+            found = 1;
+        }
+    }
+    if (found)
+    {
+        printf("задание 2) количество нечетных элементов,по модулю превосходящие A : %d\n",count);
+    }
+    else
+    {
+        printf("задание 2) Подходящие элементы не найдены\n");
+    }
+}
+void EX3(int* arr, const size_t size)
+{
+    int found = 0;
+    printf("старый массив :");
+    printArray(arr, size);
+    for  (size_t i = 0; i<size; i++)
+    {
+        if(arr[i] %3 == 0 && arr[i] %2 != 0)
+        {
+            printf("задание 3) Найден нечетный элемент кратный 3: %d\n", arr[i]);
+            int originalARR = arr[i];
+            arr[i] = arr[i] * (i + 1);
+            found = 1;
+            
+        }
+        
+    }
+    if (found)
+    {
+        printf("задание 3) Все элементы кратные 3 умножены на их порядковый номер\n");
+        printf("новый массив :");
+        printArray(arr,size);
+    }
+    else
+    {
+        printf("Элементы кратные 3 не найдены\n");
+    }
+}
